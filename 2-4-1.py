f52ff318a4255feba71e14569ebd62a265d82c6d
julia <s24025@std.it-college.ac.jp>
2024-07-03 07:59:38 +0000

    240703_PG1_p45~p54

---
+++ 2-4-1.py
@@ -0,0 +1,12 @@
+name=input("お名前は？")
+
+print(name)
+
+print(len(name))
+
+name_iter=iter(name)
+print(next(name_iter))
+print(next(name_iter))
+print(next(name_iter))
+print(next(name_iter))
+print(next(name_iter))
---
+++ 2-5.py
@@ -0,0 +1,9 @@
+print('abc')
+
+print("あいう")
+
+print("12345")
+
+print(type("12345"))
+
+print("I'm fine")
