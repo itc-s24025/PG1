6960f4f68b86f19d5af4875b06bf89a240e32421
julia <s24025@std.it-college.ac.jp>
2024-07-04 07:59:51 +0000

    240704_p54-p86

---
+++ 2-6.py
@@ -0,0 +1,17 @@
+a='abc'
+b='xyz'
+print("{} {}".format(a,b))
+
+title='name'
+value='John Smith'
+print( 'your {} is {}.'.format(title,value))
+
+age=20
+print('your age is {}.'.format(age))
+
+title1='IQ'
+value1=100
+print('{}={}'.format(title1,value1))
+
+a,b="hello","world"
+print(f"{a} {b}")
---
+++ 2-9.py
@@ -0,0 +1,7 @@
+my_list = [0, 2, 4, 6, 8 ,10]
+print(2 in my_list)
+
+my_str = [0, 2, 4, 6 ,8 ,10]
+print(3 in my_list)
+
+
