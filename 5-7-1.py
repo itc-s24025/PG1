b8192b58d8fa27bad4bac450db40d67de46a5339
julia <s24025@std.it-college.ac.jp>
2024-07-23 07:51:51 +0000

    p235-p256

---
+++ 5-7-1.py
@@ -0,0 +1,22 @@
+data = [
+    ['0001', 'Male', 'Yamada', 'Tarou', 25, 'Tokyo'],
+    ['0002', 'Male', 'Satou', 'Takeshi', 27, 'Kanagawa'],
+    ['0003', 'Female', 'Tanaka', 'Yuko', 25, 'Saitama'],
+    ['0004', 'Male', 'Suzuki', 'Ichirou', 35, 'Hokkaido']
+]
+
+
+# 辞書変数生成
+member_infomation = {}
+
+# 表データをレコード毎に格納する
+for record in data:
+    key = record[0]
+    info = record[1:]
+    member_infomation[key] = info
+
+
+# 結果を表示する
+print('number', 'infomation', sep='\t')
+for key, info in member_infomation.items():
+    print(key, info)
